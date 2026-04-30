84e282a8c170d1e0585dca4bd13c73156dfaf984
NathanVandormael1996 <vandormael.nathan@gmail.com>
2026-04-30 12:04:01 +0200

    Add github and google login

---
+++ app/Actions/HandleSocialLoginAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+use App\Enums\UserRole;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Contracts\User as SocialUser;
+use Illuminate\Support\Str;
+
+class HandleSocialLoginAction
+{
+    public function execute(SocialUser $socialUser, string $provider): void
+    {
+        $user = User::updateOrCreate([
+            'email' => $socialUser->getEmail(),
+        ], [
+            'name' => $socialUser->getName() ?? $socialUser->getNickname(),
+            'social_id' => $socialUser->getId(),
+            'social_type' => $provider,
+            'avatar' => $socialUser->getAvatar(),
+            'password' => Hash::make(Str::random(24)), // Random password for security
+            'email_verified_at' => now(),
+        ]);
+
+        if (!$user->role) {
+            $user->update(['role' => UserRole::CUSTOMER]);
+        }
+
+        Auth::login($user);
+    }
+}
