98a36a69a3c0acbb1790b00b789e64eb917d495c
NathanVandormael1996 <vandormael.nathan@gmail.com>
2026-04-30 14:08:27 +0200

    No unnecessary files

--- storage/framework/views/258fbd2c311195ed05ee84664b8c6aef.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn-sentry-primary bg-sentry-pink hover:bg-sentry-pink/80 px-6 py-2.5 text-xs shadow-[0_0_20px_rgba(240,111,151,0.2)]'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /srv/http/laravel-riftbound-exam/resources/views/components/danger-button.blade.php ENDPATH**/ ?>
--- storage/framework/views/cd4bafb8146c12648705539bad523df5.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn-sentry-primary px-6 py-2.5 text-xs'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /srv/http/laravel-riftbound-exam/resources/views/components/primary-button.blade.php ENDPATH**/ ?>
